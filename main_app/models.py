bda5c54a8327df5156bb210cc15e83fd5b2149f1
mymy209 <myramaronhime@gmail.com>
2020-09-16 22:01:41 -0700

    model revision and css

--- main_app/models.py
+++ main_app/models.py
@@ -5,6 +5,12 @@ from django.urls import reverse
 
 class Finch(models.Model):
     name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=25)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
 
     def get_absolute_url(self):
         return reverse('finches_detail', kwargs={'finch_id': self.id})
