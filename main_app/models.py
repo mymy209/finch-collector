223292fe5b4d7a8ca21478ed294a67d84e575cc2
mymy209 <myramaronhime@gmail.com>
2020-09-16 21:02:47 -0700

    index and add

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, CreateView
+from .models import Finch
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+class FinchList(ListView):
+    model = Finch
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+    success_url = '/finches/'
+
